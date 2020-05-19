0fcc7ef24edc71595973ddf7408dd44cc463d73d
empty-works <pama.mark@gmail.com>
2020-05-19 03:50:49 +0200

    Implementd CleanString function which returns a string without commas, periods etc.

--- main.cpp
+++ main.cpp
@@ -12,8 +12,24 @@ bool display_part_1() {
 		return false;
 	}
 	std::map<std::string, int> word_map_1 {};
-		
+	std::string line {};
+	while(in_file >> line) {
+			
+	}		
 };
+
+std::string CleanString(const std::string &s) {
+	std::string result;
+	for(char c : s) {
+		if(c == '.' || c == ',' || c == ';' || c == ':') {
+			continue;
+		}
+		else {
+			result += c;
+		}
+	}
+	return result;
+}
 
 void display_part_2() {
 
