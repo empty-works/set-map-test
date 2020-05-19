99c06a463258b25fc702dc8024fa1bbf1d1521a7
empty-works <pama.mark@gmail.com>
2020-05-19 03:03:29 +0200

    Open text file and added a string, int map.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <set>
 #include <map>
 #include <fstream>
+#include <algorithm>
+#include <string>
+
+bool display_part_1() {
+	std::ifstream in_file {"oz_text.txt"};	
+	if(!in_file) {
+		std::cout << "Error opening file." << std::endl;
+		return false;
+	}
+	std::map<std::string, int> word_map_1 {};
+		
+};
+
+void display_part_2() {
+
+};
 
 int main() {
 	
